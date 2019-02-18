c0269886ee526d974d12869d6e6290f0412e4a06
dAVE Inden <davidinden@gmail.com>
2019-02-17 21:20:28 -0800

    updated list of monsters and descriptions

--- src/Game/Monsters/MonsterCreate.js
+++ src/Game/Monsters/MonsterCreate.js
@@ -1,4 +1,4 @@
 export default () => {
-    const descriptors = ['Flying', 'Water', 'Fire', 'Evil', 'Angry' ]
-    const monsters = ['Bear', 'Demon', 'Skull', 'Beetle', 'Ogre', 'Robot', 'Allocamelus'];
+    const descriptors = ['Flying', 'Water', 'Fire', 'Evil', 'Angry', 'Terrifying', 'Putrid', 'Sickly', 'Roaring' ]
+    const monsters = ['Bear', 'Demon', 'Skull', 'Beetle', 'Ogre', 'Robot', 'Allocamelus', 'Screech', 'Encantado', 'Selkie', 'Scorpion', 'Bat', 'Minotaur', 'Werewolf', 'Werehyena', 'Phantom', 'Zombie', 'Wyvern'];
 }
