018bf03a2d376e0436df767af5d3f79d52ba02d3
Perdeep Singh <8705702+perdeepOsingh@users.noreply.github.com>
2019-08-24 16:41:47 +1000

    - Added decision making for her to decide if he/she wants to enter in dungeon or not.

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import ChatBot from 'react-simple-chatbot';
 import GameInfo from './GameInfo/GameInfo';
-import createMonster from './Monsters/MonsterCreate';
 import PlayerInfo from './Player/PlayerInfo';
 
 class Game extends Component {
@@ -10,57 +9,94 @@ class Game extends Component {
         this.state = {
             score: 0,
             player: new PlayerInfo(),
+            enterInDungeon: null,
             isPlayerAlive: true,
             currentMonster: undefined,
-            currentlyFighting: false
+            currentlyFighting: false,
+            steps: [
+                {
+                    id: `game-start`,
+                    message: `Welcome adventurer! Thank you for coming to save us from these terrible monsters. What is your name?`,
+                    trigger: `player-create`
+                },
+                {
+                    id: `player-create`,
+                    user: true,
+                    validator: (value) => {
+                    if (value.length > 20) {
+                        return 'Name too long. Keep it to 20 characters or less.';
+                    } else {
+                        return true;
+                    }
+                },
+                    trigger: `hello-hero`
+                },
+                {
+                    id: `hello-hero`,
+                    message: ({previousValue}) => {
+                        this.setState({ player: { name: previousValue }});
+                        return `Greetings ${previousValue}. We are so happy you have come to help us. The dungeon awaits you, who knows what lurks in the darkness...`
+                    },
+                    trigger: `enter-dungeon`
+                },
+                {
+                    id: `enter-dungeon`,
+                    message: `Ready to enter in dungeon?`,
+                    trigger: `hero-courage-test`
+                },
+                {
+                    id: `hero-courage-test`,
+                    options: [
+                        { value: true, label: 'Yes', trigger: 'enter-quest-decision' },
+                        { value: false, label: 'No', trigger: 'enter-quest-decision' }
+                    ]
+                },
+                {
+                    id: `enter-quest-decision`,
+                    message: ({previousValue}) => {
+                        if (previousValue) {
+                            this.setState({player: {name: `${this.state.player.name} the brave`}, enterInDungeon: true});
+                            return `Good Luck`
+                        }
+
+                        this.setState({player: {name: `${this.state.player.name} Chicken`}, enterInDungeon: false});
+                        return `Disappointing...`
+                    },
+                    trigger: () => {
+                        if (this.state.enterInDungeon) {
+                            return `welcome-to-dungeon`
+                        }
+                        return `end-quest`
+                    }
+                },
+                {
+                    id: `welcome-to-dungeon`,
+                    component: (
+                        <div>coming soon....</div>
+                    ),
+                    end: true,
+                },
+                {
+                    id: `end-quest`,
+                    component: (
+                        <div>Monster won</div>
+                    ),
+                    end: true,
+                }
+            ]
         }
     }
 
     render() {
+        const {score, player} = this.state
         return (
-            <>
-                <GameInfo score={this.state.score} player={this.state.player} />
-                <ChatBot
-                    width={`600px`}
-                    // speechSynthesis={{ enable: true, lang: 'en' }}
-                    steps={
-                        [
-                            {
-                              id: `game-start`,
-                              message: `Welcome adventurer! Thank you for coming to save us from these terrible monsters. What is your name?`,
-                              trigger: `player-create`
-                            },
-                            {
-                              id: `player-create`,
-                              user: true,
-                              validator: (value) => {
-                                if (value.length > 20) {
-                                    return 'Name too long. Keep it to 20 characters or less.';
-                                } else {
-                                    return true;
-                                }
-                            },
-                              trigger: `hello-hero`
-                            },
-                            {
-                              id: `hello-hero`,
-                              message: ({ previousValue }) => {
-                                this.setState({ player: { name: previousValue }});
-                                return `Greetings ${previousValue}. We are so happy you have come to help us. The dungeon awaits you, who knows what lurks in the darkness...`
-                              },
-                              trigger: `enter-dungeon`
-                            },
-                            {
-                                id: `enter-dungeon`,
-                                component: (
-                                    <div> The Dungeon of Horrors </div>
-                                ),
-                                end: true
-                            }
-                          ]
-                    }
+            <React.Fragment>
+            <GameInfo score={score} player={player} />
+            <ChatBot
+                width={`600px`}
+                steps={this.state.steps}
                 />
-            </>
+            </React.Fragment>
         )
     }
 
